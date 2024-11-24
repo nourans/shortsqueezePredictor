603b549f99df5f4ded1cf425d5602ea40540d3b5
Nouran Sakr <nsakr2@uwo.ca>
2024-11-24 14:49:28 -0500

    third commit

--- alphaVantageAPI.py
+++ alphaVantageAPI.py
@@ -1,9 +1,11 @@
+# this is intradaily data
+
 import requests
 import json
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 _url1 = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="
-_symbol = "PLUG" # this should be dynamic
+_symbol = "AMC" # this should be dynamic
 _url2 = "&interval=5min&month=2009-01&outputsize=full&apikey=6KX0TXEWMWVOT1NK"
 
 _fullURL = _url1 + _symbol + _url2
